6d2fb5572a36ad7b59556d2056e6621f651519da
mcapizzi <mcapizzi@email.arizona>
2015-08-05 11:46:44 -0700

    recursion still not working properly

--- MergeSort/mergeSort.py
+++ MergeSort/mergeSort.py
@@ -4,19 +4,26 @@ __author__ = 'mcapizzi'
 import halfing as h
 import merge as m
 
+def runMergeSort(z):
+    sortedA = []
+    sortedB = []
+    mergeSort(z)
 
 def mergeSort(z):
-    sortedArray = []
     if len(z) > 1:
         print("halfing ", z)
         a, b = h.halfing(z)
-        print("recursion")
+        print("running mergeSort on ", a)
         mergeSort(a)
+        print("running mergeSort on ", b)
         mergeSort(b)
         print("merging", a, b)
         c = m.merge(a, b)
         print("merged", c)
-        print("adding", c, "to sortedArray")
-        for i in c:
-        sortedArray.append(i)
-    return sortedArray
+    else:
+        a, b = h.halfing(z)
+        c = m.merge(a, b)
+        print("merge", c)
+
+
+
