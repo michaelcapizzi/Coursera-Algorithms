543940c250177825986c7d9a4baf2c99f86adce7
mcapizzi <mcapizzi@email.arizona>
2015-08-06 15:08:29 -0700

    can't figure out error importing halfing module from other directory

--- CountInversions/countInversions.py
+++ CountInversions/countInversions.py
@@ -1,13 +1,17 @@
 __author__ = 'mcapizzi'
 
+# import sys
+# sys.path.append("../MergeSort")
+
 import mergeAndCount as mc
-from MergeSort.halfing import halfing as h
+from MergeSort.halfing import halfing           #I'm getting an import error.  Why?
+
 
 def countInversions(z, n):
     if len(z) == 1:
         return 0
     else:
-        l, r = h.halfing(z)
+        l, r = halfing(z)
         lSorted, lCount = countInversions(l, n)
         rSorted, rCount = countInversions(r, n)
         sMerged, sCount = mc.mergeAndCount(z, n)
